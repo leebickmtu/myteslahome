8f661be52818b49cfc915c48a76b624feb69a25c
Korbin Bickel <leebickmtu@gmail.com>
2022-06-02 08:44:09 -0400

    Change publicPath

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
-  publicPath: process.env.NODE_ENV === 'production' ? '/dist/' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/myteslahome/dist/' : '/',
 
   pluginOptions: {
     vuetify: {
