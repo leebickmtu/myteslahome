4e3254d654ce494084f11095e4843dd3d7caaa9c
Korbin Bickel <leebickmtu@gmail.com>
2022-06-02 08:37:03 -0400

    Set publicPath for dist build

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  publicPath: process.env.NODE_ENV === 'production' ? '/dist/' : '/',
+
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
